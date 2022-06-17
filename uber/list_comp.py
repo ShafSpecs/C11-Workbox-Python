3e6bec5688466adcc17ed6c04f2fc6cb08a1791d
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-06-17 10:21:47 +0100

    List comprehension

---
+++ uber/list_comp.py
@@ -0,0 +1,23 @@
+#####
+# LIST COMPREHENSION
+# Constructing new arrays from an iterable(lists, ranges, etc.)
+#####
+
+lst = [x for x in range(1, 6)]
+print(lst)
+
+lst2 = list(map(lambda x: x * 10, [i for i in range(1, 4)]))
+print(lst2)
+
+lst3 = list(range(1, 11))
+print(lst3)
+
+lst4 = list(map(lambda i: i * 2, range(1, 11)))
+print(lst4)
+
+### Square the values of even numbers between 1-100 and just print the odd ones, i.e [1, 4, 3, 16,...].
+play = [x**2 if x % 2 == 0 else x for x in range(1, 101)]
+print(play)
+
+play2 = list(map(lambda i: i**2 if i % 2 == 0 else i, range(1, 101)))
+print(play2)
