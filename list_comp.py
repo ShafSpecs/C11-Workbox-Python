d99683ea9796fc353a4e5c53c563bf433a578782
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-06-17 10:55:02 +0100

    Moved test file from uber 😝

--- list_comp.py
+++ list_comp.py
@@ -15,9 +15,18 @@ print(lst3)
 lst4 = list(map(lambda i: i * 2, range(1, 11)))
 print(lst4)
 
+
 ### Square the values of even numbers between 1-100 and just print the odd ones, i.e [1, 4, 3, 16,...].
 play = [x**2 if x % 2 == 0 else x for x in range(1, 101)]
 print(play)
 
 play2 = list(map(lambda i: i**2 if i % 2 == 0 else i, range(1, 101)))
 print(play2)
+
+
+work = [i for i in range(2, 100)]
+print(work)
+
+
+user_input = [int(input("Enter a grade: ")) for i in range(1, 6)]
+print(user_input)
